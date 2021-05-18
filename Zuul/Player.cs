32fae9ec24febb47486c49f60314a1b50c0e6063
Emiel Ridder <emiel.ridder15@gmail.com>
2021-05-18 10:24:54 +0200

    test

---
+++ Zuul/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zuul
+{
+    class Player
+    {
+        public Room currentRoom {get; set;}
+
+        public Player()
+        {
+            currentRoom = null;
+        }
+    }
+}
